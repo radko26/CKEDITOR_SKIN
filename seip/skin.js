fd6bcc280f938c26543faeda10ffab7e091f4a66
Radoslav Petkov <radoslav.petkov@sirma.bg>
2015-03-27 13:36:58 +0200

    initial commit

---
+++ seip/skin.js
@@ -0,0 +1,8 @@
+﻿/*
+ Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ For licensing, see LICENSE.md or http://ckeditor.com/license
+*/
+CKEDITOR.skin.name = "seip_skin";
+CKEDITOR.skin.ua_editor = "";
+CKEDITOR.skin.ua_dialog = "";
+CKEDITOR.skin.chameleon = function() {}();
